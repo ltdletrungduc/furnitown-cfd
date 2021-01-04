c897d4aed8bf02df7a9d4965f55c1430838f0a39
Le Duc <ltdletrungduc@gmail.com>
2021-01-04 21:22:16 +0700

    Test commit

--- js/main.js
+++ js/main.js
@@ -36,8 +36,8 @@ $(window).on("load", function () {
 });
 
 /* SHOW - HIDE HEADER WHEN SCROLLING */
-const header = $("header"),
-	sliderSection = $("main section.slider");
+const header = $("header.main-header"),
+	toggleBtn = $("#toggle-btn");
 let lastScollPos = 0;
 $(document).on("scroll", function () {
 	headerStyling($(this));
@@ -52,7 +52,7 @@ function headerStyling(window) {
 		header.removeClass("--active");
 	} else {
 		// upscroll code
-		if (scrollTop >= sliderSection.height() / 2 - header.height()) {
+		if (scrollTop >= toggleBtn.offset().top + toggleBtn.height()) {
 			header.addClass("--active");
 		} else {
 			header.removeClass("--active");
@@ -63,12 +63,15 @@ function headerStyling(window) {
 
 /* SHOW - HIDE SIDENAV WHEN CLICK BTN */
 const sideNav = $("nav"),
-	overlay = $("#overlay");
-
+	overlay = $("#overlay"),
+	mobileBurger = $("header .memu-mobile .burger");
 $(document).on("click", "#toggle-btn", function () {
 	openSideNav();
 });
 
+$(document).on("click", "#toggle-btn-mobile", function () {
+	openSideNav();
+});
 $(document).on("click", "#close-nav", function () {
 	closeSideNav();
 });
@@ -122,4 +125,48 @@ $(window).on("load", function () {
 			},
 		},
 	});
+});
+
+$(window).on("load", function () {
+	$(".project .project__types .project__type").each(function (index, value) {
+		let delay = index * 60;
+		$(this).css({
+			"animation-delay": delay + "ms",
+		});
+	});
+});
+
+// PRODUCT-IMAGE-FLICKITY
+const $productImages = $("main.product .image__wrapper");
+// bind event listener first
+$productImages.on("ready.flickity", function () {
+	console.log("Flickity ready");
+});
+// initialize Flickity
+$productImages.flickity({
+	cellSelector: ".image__item",
+	cellAlign: "left",
+	contain: true,
+	wrapAround: true, //loop
+	draggable: true,
+	prevNextButtons: false,
+	pageDots: false,
+	autoPlay: false,
+	pauseAutoPlayOnHover: false,
+	selectedAttraction: 0.01, //Higher attraction makes the slider move faster
+	friction: 0.2, //Higher friction makes the slider feel stickier and less bouncy
+	setGallerySize: false, //Set size of cells by CSS
+	// groupCells: 3,
+	on: {
+		ready: function () {
+			// let default_dots = $(".flickity-page-dots"),
+			// 	my_dots = $(".slider__paging");
+			// default_dots.addClass("slider__dots");
+			// default_dots.appendTo(my_dots);
+		},
+		change: function (index) {
+			// let number = $(".slider__number span");
+			// number.text((index + 1).toString().padStart(2, 0));
+		},
+	},
 });
